50d1f0df0d1eb4d35d43b14d137967604d4d52dd
ulfw <ulf.worsoe@mosek.com>
2017-08-25 14:33:00 +0200

    initial package

---
+++ test/runtests.jl
@@ -0,0 +1,24 @@
+using MathOptInterface
+using Base.Test
+
+const MOI = MathOptInterface
+
+include(joinpath(Pkg.dir("MathOptInterface"),"test","contlinear.jl"))
+@testset "Continuous linear problems" begin
+    contlineartest(MathOptMosekSolver(QUIET = true))
+end
+
+# include("contquadratic.jl")
+# @testset "Continuous quadratic problems" begin
+#     # contquadratictest(GurobiSolver())
+# end
+
+include(joinpath(Pkg.dir("MathOptInterface"),"test","contconic.jl"))
+@testset "Continuous conic problems" begin
+    contconictest(MathOptMosekSolver(QUIET = true))
+end
+
+include(joinpath(Pkg.dir("MathOptInterface"),"test","intlinear.jl"))
+@testset "Mixed-integer linear problems" begin
+    intlineartest(MathOptMosekSolver(QUIET = true))
+end
